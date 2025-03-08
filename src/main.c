afbaea5b18e90237183920e7763d6d2c5a7a9525
clairedwl <claire2walle@gmail>
2025-03-08 21:21:22 +0100

    led blinkblink ✨

--- src/main.c
+++ src/main.c
@@ -1,1 +1,20 @@
-void app_main() {}
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#define LED_PIN 8
+
+void led_blink(void *pvParams) {
+    esp_rom_gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction (LED_PIN,GPIO_MODE_OUTPUT);
+    while (1) {
+        gpio_set_level(LED_PIN,0);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        gpio_set_level(LED_PIN,1);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
+
+void app_main() {
+    xTaskCreate(&led_blink,"LED_BLINK",512,NULL,5,NULL);
+}
